3a8a17bb4085a3721045e29d60d3bf8fd49d4940
osvaldocs <pablo_campos92@hotmail.com.ar>
2023-10-29 13:38:50 -0300

    clone el repositorio github a mi repositorio local y agregue archivos

---
+++ src/ejerciciosJavaBasico/Ejercicio1.java
@@ -0,0 +1,15 @@
+package ejerciciosJavaBasico;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args){
+
+        System.out.println("int");
+        System.out.println("long");
+        System.out.println("double");
+        System.out.println("boolean");
+        System.out.println("String");
+
+    }
+
+}
