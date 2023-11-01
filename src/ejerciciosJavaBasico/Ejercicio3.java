dcc5594e07cb0c176d374b4493cfa67e5af55ff4
osvaldocs <pablo_campos92@hotmail.com.ar>
2023-11-01 12:44:58 -0300

    Agregue el ejercicio3

---
+++ src/ejerciciosJavaBasico/Ejercicio3.java
@@ -0,0 +1,15 @@
+package ejerciciosJavaBasico;
+
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+
+        String[] primos = {"Alma", "Ciro", "Jacobo", "Vera"};
+
+        for (String primo : primos){
+            System.out.print(primo + " ");
+        }
+
+
+    }
+}
